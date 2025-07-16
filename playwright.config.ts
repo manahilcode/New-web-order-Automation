6a3b06c8718191572d9d7e9b20922443220963e0
TECHNIFI <manahiladil137@gmail.com>
2025-07-16 13:21:55 +0500

    change playwright.config

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,11 +2,17 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',  // Location of your test files
-  timeout: 30000,      // Max time for each test
+     
   retries: 1,          // Retry on failure
   use: {
-    headless: true,            // Run browser in headless mode
+    
+    headless:true,            // Run browser in headless mode
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
   },
+  reporter: [
+    ['html', { outputFolder: 'playwright-report' }],
+    ['json', { outputFile: 'playwright-report/playwright-report.json' }],
+    ['junit', { outputFile: 'results.xml' }],
+  ],
 });
